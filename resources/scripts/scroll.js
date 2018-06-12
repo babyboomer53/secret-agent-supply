67d9c0ac85b7b35060fd5969f3fa65fc12174850
babyboomer53 <edgar.cole@san.rr.com>
2018-06-12 16:42:23 -0700

    It appears as though I finally got the hover selectors to work.

--- resources/scripts/scroll.js
+++ resources/scripts/scroll.js
@@ -2,15 +2,15 @@
 JSLint complained loudly about the content of this file, but I don't yet
 have the expertise to make the corrections.
 */
-$(document).ready(function() {
+$(document).ready(function () {
 
 	var scrolllink = $('a');
 
 	// Smooth scrolling
 
-	scrolllink.click(function(event) {
+	scrolllink.click(function (event) {
 		event.preventDefault();
-		$('body, html').animate ({
+		$('body, html').animate({
 			scrollTop: $(this.hash).offset().top
 		}, 2000);
 	});
